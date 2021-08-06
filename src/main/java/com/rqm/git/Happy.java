b036be485a86d0c14ddd7b45501b0b5586949091
eclipse_user <eclipse_user@163.com>
2021-08-06 11:25:32 +0800

    test commit

--- src/main/java/com/rqm/git/Happy.java
+++ src/main/java/com/rqm/git/Happy.java
@@ -2,7 +2,8 @@ package com.rqm.git;
 
 public class Happy {
   public static void main(String[] args) {
-	System.out.println("Right……");
+	 System.out.println("Right……");
 	 System.out.println("left……");
+	 System.out.println("测试修改是否将真的推送到远程库");
 }
 }
